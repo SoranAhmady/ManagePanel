463dd37c5590813f76006e2209bae5597bf8a454
mr-rahin <zzrahin7@gmail.com>
2025-09-21 00:49:33 +0330

    feat(footer) footer ui complete

---
+++ src/component/ui/organisms/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import { quikAcssecFooter, tarkhineShab, titleQuik } from "../../../../core/constans/quikAcssecFooter";
+import { FooterContactUs } from "../../moloculse/FooterContactUs/FooterContactUs";
+import { FooterItemBar } from "../../moloculse/FooterItemBar/FooterItemBar";
+import { motion } from "framer-motion";
+export function Footer() {
+    return (
+        <motion.footer
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6}}
+            className="bg-[url('/assets/p4.jpg')] bg-black/55 bg-blend-multiply flex justify-around gap-16 flex-row-reverse pb-20 p-12 px-24 bg-cover bg-center">
+            <div>
+                <FooterItemBar title="دسترسی آسان" item={quikAcssecFooter} icon={titleQuik} />
+            </div>
+            <div>
+                <FooterItemBar title="شعبه‌های ترخینه" item={tarkhineShab} />
+            </div>
+            <div className=" w-[45%]">
+                <FooterContactUs />
+            </div>
+        </motion.footer>
+    )
+}
